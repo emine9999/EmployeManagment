48aaa838d2a8fa13119afb3748bcfe9399dd5ccc
Amine Elhasbi <amineelhasbi23@gmail.com>
2024-10-28 09:18:35 +0100

    initialisation du projet

---
+++ EmployeManagment/src/main/java/com/ensa/EmployeManagment/EmployeManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.ensa.EmployeManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeManagmentApplication.class, args);
+	}
+
+}
---
+++ EmployeManagment/src/test/java/com/ensa/EmployeManagment/EmployeManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensa.EmployeManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
